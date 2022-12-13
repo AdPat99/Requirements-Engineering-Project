3b0d662b0c0ce2734b3ad17c8452ebfd4de7527e
JhonDCurse <103522477+JhonDCurse@users.noreply.github.com>
2022-12-13 20:59:46 +0000

    Update to several pages.

--- prototipo-er/src/pages/Contacts.js
+++ prototipo-er/src/pages/Contacts.js
@@ -4,6 +4,17 @@ export default function Contacts() {
   return (
     <>
       <h1>Contacts</h1>
+    <body>
+        <h1>Priority Contacts</h1>
+<div>
+        <div class="contact list">
+            <span>Name 1 - 170393654</span>
+            <button class='callbtn'>Call Icon</button>
+            <span></span>
+        </div>
+        <button>Add contact</button>
+</div>
+    </body>
     </>
   )
 }
--- prototipo-er/src/pages/Customizations.js
+++ prototipo-er/src/pages/Customizations.js
@@ -4,6 +4,8 @@ export default function Customizations() {
   return (
     <>
       <h1>Customizations</h1>
+        <button>Playlist</button>
+        <button>AC temperature</button>
     </>
   )
 }
