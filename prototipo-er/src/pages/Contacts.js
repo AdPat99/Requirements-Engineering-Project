7eed49ca684c9d705375c98de8fbb8d213ed0ceb
JhonDCurse <103522477+JhonDCurse@users.noreply.github.com>
2022-12-14 08:36:22 +0000

    Update Contacts.js

--- prototipo-er/src/pages/Contacts.js
+++ prototipo-er/src/pages/Contacts.js
@@ -8,9 +8,20 @@ export default function Contacts() {
         <h1>Priority Contacts</h1>
 <div>
         <div class="contact list">
-            <span>Name 1 - 170393654</span>
+            <span>Name 1 - 170393654  </span>
             <button class='callbtn'>Call Icon</button>
-            <span></span>
+        </div>
+        <div class="contact list">
+            <span>Name 2 - 170675938  </span>
+            <button class='callbtn'>Call Icon</button>
+        </div>
+        <div class="contact list">
+            <span>Name 3 - 170639562  </span>
+            <button class='callbtn'>Call Icon</button>
+        </div>
+        <div class="contact list">
+            <span>Name 2 - 170867394  </span>
+            <button class='callbtn'>Call Icon</button>
         </div>
         <button>Add contact</button>
 </div>
