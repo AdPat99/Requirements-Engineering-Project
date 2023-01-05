112bf17b436e2127c3b58dcaa8090320e9fbb6c6
JhonDCurse <103522477+JhonDCurse@users.noreply.github.com>
2023-01-05 18:39:02 +0000

    html files (empty for now), more code for Calendar

--- prototipo-er/src/pages/Calender.js
+++ prototipo-er/src/pages/Calender.js
@@ -1,5 +1,21 @@
 import React from 'react'
 
+let options = document.querySelectorAll('.griditem');
+
+let i;
+function unselectAll() {
+    for (i = 0; i < options.length; i++) {
+        options[i].style.backgroundColor = "";
+    }
+}
+
+// loop through the options and add the event listener to each element
+options.forEach(griditem => {
+  griditem.addEventListener('click', function(){
+      unselectAll();
+      this.style.backgroundColor = "#333";  
+   } );
+});
 
 export default function Calender() {
   return (
@@ -13,35 +29,35 @@ export default function Calender() {
       <h2>Setembro</h2>
       <div class="grid-container">
       <button class="btn" onclick="btnColor()">1</button>
-  <div class="grid-item">2</div>
-  <div class="grid-item">3</div>  
-  <div class="grid-item">4</div>
-  <div class="grid-item">5</div>
-  <div class="grid-item">6</div>  
-  <div class="grid-item">7</div>
-  <div class="grid-item">8</div>
-  <div class="grid-item">9</div>  
-  <div class="grid-item">10</div> 
-  <div class="grid-item">11</div>   
-  <div class="grid-item">12</div> 
-  <div class="grid-item">13</div> 
-  <div class="grid-item">14</div> 
-  <div class="grid-item">15</div> 
-  <div class="grid-item">16</div> 
-  <div class="grid-item">17</div> 
-  <div class="grid-item">18</div> 
-  <div class="grid-item">19</div> 
-  <div class="grid-item">20</div> 
-  <div class="grid-item">21</div> 
-  <div class="grid-item">22</div> 
-  <div class="grid-item">23</div> 
-  <div class="grid-item">24</div> 
-  <div class="grid-item">25</div> 
-  <div class="grid-item">26</div> 
-  <div class="grid-item">27</div> 
-  <div class="grid-item">28</div> 
-  <div class="grid-item">29</div> 
-  <div class="grid-item">30</div> 
+  <div class="griditem">2</div>
+  <div class="griditem">3</div>  
+  <div class="griditem">4</div>
+  <div class="griditem">5</div>
+  <div class="griditem">6</div>  
+  <div class="griditem">7</div>
+  <div class="griditem">8</div>
+  <div class="griditem">9</div>  
+  <div class="griditem">10</div> 
+  <div class="griditem">11</div>   
+  <div class="griditem">12</div> 
+  <div class="griditem">13</div> 
+  <div class="griditem">14</div> 
+  <div class="griditem">15</div> 
+  <div class="griditem">16</div> 
+  <div class="griditem">17</div> 
+  <div class="griditem">18</div> 
+  <div class="griditem">19</div> 
+  <div class="griditem">20</div> 
+  <div class="griditem">21</div> 
+  <div class="griditem">22</div> 
+  <div class="griditem">23</div> 
+  <div class="griditem">24</div> 
+  <div class="griditem">25</div> 
+  <div class="griditem">26</div> 
+  <div class="griditem">27</div> 
+  <div class="griditem">28</div> 
+  <div class="griditem">29</div> 
+  <div class="griditem">30</div> 
 </div>
     </>
   )
