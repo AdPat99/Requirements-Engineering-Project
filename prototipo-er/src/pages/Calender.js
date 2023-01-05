6b5f0497427b99bd776e2da0ca7fb4cfa0134552
JhonDCurse <103522477+JhonDCurse@users.noreply.github.com>
2023-01-05 23:32:43 +0000

    Removal of html files. Calendar and Contacts code.

--- prototipo-er/src/pages/Calender.js
+++ prototipo-er/src/pages/Calender.js
@@ -9,11 +9,16 @@ function unselectAll() {
     }
 }
 
+function show() {
+  document.getElementById("marker").classList.toggle("show");
+}
+
 // loop through the options and add the event listener to each element
 options.forEach(griditem => {
   griditem.addEventListener('click', function(){
       unselectAll();
-      this.style.backgroundColor = "#333";  
+      this.style.backgroundColor = "#333";
+
    } );
 });
 
@@ -28,36 +33,36 @@ export default function Calender() {
       
       <h2>Setembro</h2>
       <div class="grid-container">
-      <button class="btn" onclick="btnColor()">1</button>
-  <div class="griditem">2</div>
-  <div class="griditem">3</div>  
-  <div class="griditem">4</div>
-  <div class="griditem">5</div>
-  <div class="griditem">6</div>  
-  <div class="griditem">7</div>
-  <div class="griditem">8</div>
-  <div class="griditem">9</div>  
-  <div class="griditem">10</div> 
-  <div class="griditem">11</div>   
-  <div class="griditem">12</div> 
-  <div class="griditem">13</div> 
-  <div class="griditem">14</div> 
-  <div class="griditem">15</div> 
-  <div class="griditem">16</div> 
-  <div class="griditem">17</div> 
-  <div class="griditem">18</div> 
-  <div class="griditem">19</div> 
-  <div class="griditem">20</div> 
-  <div class="griditem">21</div> 
-  <div class="griditem">22</div> 
-  <div class="griditem">23</div> 
-  <div class="griditem">24</div> 
-  <div class="griditem">25</div> 
-  <div class="griditem">26</div> 
-  <div class="griditem">27</div> 
-  <div class="griditem">28</div> 
-  <div class="griditem">29</div> 
-  <div class="griditem">30</div> 
+  <div id="day1" class="griditem">1</div>
+  <div id="day2" class="griditem">2</div>
+  <div id="day3" class="griditem">3</div>  
+  <div id="day4" class="griditem">4</div>
+  <div id="day5" class="griditem">5</div>
+  <div id="day6" class="griditem">6</div>  
+  <div id="day7" class="griditem">7</div>
+  <div id="day8" class="griditem">8</div>
+  <div id="day9" class="griditem">9</div>  
+  <div id="day10" class="griditem">10</div> 
+  <div id="day11" class="griditem">11</div>   
+  <div id="day12" class="griditem">12</div> 
+  <div id="day13" class="griditem">13</div> 
+  <div id="day14" class="griditem">14</div> 
+  <div id="day15" class="griditem">15</div> 
+  <div id="day16" class="griditem">16</div> 
+  <div id="day17" class="griditem">17</div> 
+  <div id="day18" class="griditem">18</div> 
+  <div id="day19" class="griditem">19</div> 
+  <div id="day20" class="griditem">20</div> 
+  <div id="day21" class="griditem">21</div> 
+  <div id="day22" class="griditem">22</div> 
+  <div id="day23" class="griditem">23</div> 
+  <div id="day24" class="griditem">24</div> 
+  <div id="day25" class="griditem">25</div> 
+  <div id="day26" class="griditem">26</div> 
+  <div id="day27" class="griditem">27</div> 
+  <div id="day28" class="griditem">28</div> 
+  <div id="day29" class="griditem">29</div> 
+  <div id="day30" class="griditem">30</div> 
 </div>
     </>
   )
--- prototipo-er/src/pages/Contacts.js
+++ prototipo-er/src/pages/Contacts.js
@@ -1,29 +1,42 @@
 import React from 'react'
+function addContact() {
+document.getElementById("list").innerHTML +=
+            "<div>This is the text which has been inserted by JS</div>";
+}
+
+function remContact() {
+    let element = document.getElementById("contact1");
+element.remove();
+}
 
 export default function Contacts() {
   return (
     <>
     <body>
         <h1>Priority Contacts</h1>
-<div>
+<div id="list">
         <div class="contact list">
-            <span>Name 1 - 170393654  </span>
-            <button class='callbtn'>Call Icon</button>
+            <span>Albert Jhonson - 170393654  </span>
+            <button class='callbtn'>Call</button>
+            <button id="contact1" class='callbtn' onClick={remContact}>Delete contact</button>
         </div>
         <div class="contact list">
-            <span>Name 2 - 170675938  </span>
-            <button class='callbtn'>Call Icon</button>
+            <span>Mike Andreson - 170675938  </span>
+            <button class='callbtn'>Call</button>
+            <button class='callbtn'>Delete contact</button>
         </div>
         <div class="contact list">
-            <span>Name 3 - 170639562  </span>
-            <button class='callbtn'>Call Icon</button>
+            <span>Anabella Rick - 170639562  </span>
+            <button class='callbtn'>Call</button>
+            <button class='callbtn'>Delete contact</button>
         </div>
         <div class="contact list">
-            <span>Name 2 - 170867394  </span>
-            <button class='callbtn'>Call Icon</button>
+            <span>Emily Alberta - 170867394  </span>
+            <button class='callbtn'>Call</button>
+            <button class='callbtn'>Delete contact</button>
         </div>
-        <button>Add contact</button>
 </div>
+<button onClick={addContact}>Add contact</button>
     </body>
     </>
   )
