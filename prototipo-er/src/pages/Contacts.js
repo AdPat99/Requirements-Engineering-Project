814254c53265168ac91c9a1a073c55b8408a2720
JhonDCurse <103522477+JhonDCurse@users.noreply.github.com>
2023-01-05 23:57:14 +0000

    Last code

--- prototipo-er/src/pages/Contacts.js
+++ prototipo-er/src/pages/Contacts.js
@@ -1,11 +1,12 @@
 import React from 'react'
 function addContact() {
 document.getElementById("list").innerHTML +=
-            "<div>This is the text which has been inserted by JS</div>";
+            "<span>Name - Number </span>"+
+            " <button class='callbtn'>Call</button> "
+            +" <button class='callbtn'>Delete contact</button>"; 
 }
-
 function remContact() {
-    let element = document.getElementById("contact1");
+    let element = document.getElementById();
 element.remove();
 }
 
@@ -15,29 +16,38 @@ export default function Contacts() {
     <body>
         <h1>Priority Contacts</h1>
 <div id="list">
-        <div class="contact list">
+        <div class='contact list'>
             <span>Albert Jhonson - 170393654  </span>
             <button class='callbtn'>Call</button>
             <button id="contact1" class='callbtn' onClick={remContact}>Delete contact</button>
         </div>
-        <div class="contact list">
+        <div class='contact list'>
             <span>Mike Andreson - 170675938  </span>
             <button class='callbtn'>Call</button>
             <button class='callbtn'>Delete contact</button>
         </div>
-        <div class="contact list">
+        <div class='contact list'>
             <span>Anabella Rick - 170639562  </span>
             <button class='callbtn'>Call</button>
             <button class='callbtn'>Delete contact</button>
         </div>
-        <div class="contact list">
+        <div class='contact list'>
             <span>Emily Alberta - 170867394  </span>
             <button class='callbtn'>Call</button>
             <button class='callbtn'>Delete contact</button>
         </div>
 </div>
-<button onClick={addContact}>Add contact</button>
+<p>Add contact</p>
+<div><form name="frm1" method="post" onsubmit="return addContact()">
+<label for="name">Contact Name:</label>
+    <input type="text" name="name"></input>
+    <label for="number">Contact Number:</label>
+        <input type="text" name ="number"></input>
+    <input type="submit" value="Submit"></input>
+</form>
+</div>
     </body>
     </>
   )
 }
+
